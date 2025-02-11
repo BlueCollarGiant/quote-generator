dcd74cf4989c48e9bb4c5e474c340e03fffaf9b1
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-02-11 10:22:34 -0600

    setup data template and finished testing response data

---
+++ data/responses.js
@@ -0,0 +1,117 @@
+const responses = [ {
+    id: 1,
+    author: "Oscar Wilde",
+    qoute:  "Be yourself; everyone else is already taken.",
+    mood: na,
+},
+            {
+    id: 2,
+    author: "Oscar Wilde",
+    qoute:  "To live is the rarest thing in the world. Most people exist, that is all.",
+    mood:  na,
+},
+            {
+    id: 3,
+    author: "Mahatma Gandhi",
+    qoute:  "Be the change that you wish to see in the world.",
+    mood: na,
+},
+            {
+    id: 4,
+    author: "Eleanor Roosevelt",
+    qoute:  "Do what you feel in your heart to be right - for you'll be criticized anyway.",
+    mood: na,
+},
+            {
+    id: 5,
+    author: "Eleanor Roosevelt",
+    qoute:   "Do one thing every day that scares you.",
+    mood: na,
+},
+            {
+    id: 6,
+    author: "Martin Luther King",
+    qoute:  "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate; only love can do that.",
+    mood: na,
+},
+            {
+    id: 7,
+    author: "Martin Luther King",
+    qoute:  "Injustice anywhere is a threat to justice everywhere.",
+    mood: na,
+},
+
+            {
+    id: 8,
+    author: "Albert Einstein",
+    qoute:  "Weak people revenge. Strong people forgive. Intelligent People Ignore.",
+    mood: na,
+},
+
+            {
+    id: 9,
+    author: "Thomas A. Edison",
+    qoute:  "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    mood: na,
+},
+
+            {
+    id: 10,
+    author: "Thomas A. Edison",
+    qoute:  "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    mood: na,
+},
+
+            {
+    id: 11,
+    author: "George Bernard Shaw",
+    qoute:  "Life isn't about finding yourself. Life is about creating yourself.",
+    mood: na,
+},
+
+            {
+    id: 12,
+    author: "Winston Churchill",
+    qoute:  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    mood: na,
+},
+
+            {
+    id: 13,
+    author: "Mother Teresa",
+    qoute:  "Peace begins with a smile.",
+    mood: na,
+},
+
+            {
+    id: 14,
+    author: "Mother Teresa",
+    qoute:  "If you can't feed a hundred people, then feed just one.",
+    mood: na,
+},
+
+            {
+    id: 15,
+    author: "Francis of Assisi",
+    qoute:  "All the darkness in the world cannot extinguish the light of a single candle.",
+    mood: na,
+},
+
+            {
+    id: 16,
+    author: "Francis of Assisi",
+    qoute:  "A single sunbeam is enough to drive away many shadows.",
+    mood: na,
+},
+]
+
+
+
+// setting up data templete
+/*{
+    id: ,
+    author: ,
+    qoute:  ,
+    mood: na,
+},*/
+// this is my teplate for new qoutes
---
+++ data/usersubmits.js
@@ -0,0 +1,18 @@
+const userSubmits = [{
+    
+        id: ,
+        author: ,
+        qoute:  ,
+        mood: na,
+    },
+
+
+]
+
+// current template for data.
+/* {
+    id: ,
+    author: ,
+    qoute:  ,
+    mood: na,
+} */
