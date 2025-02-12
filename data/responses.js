19ede75b9b84da1d921c54233cbe172682ab9c19
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-02-12 14:14:31 -0600

    added file tree graphic and a little cleanup

--- data/responses.js
+++ data/responses.js
@@ -125,7 +125,7 @@ function generateCard(quote) {
         <img src="https://plus.unsplash.com/premium_photo-1675701826925-710e336bbba5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         <p class="quote">${quote.quote}</p>
         <p class="author">— ${quote.author}</p>
-        <button class="generate-new-quote"> Get a New quote</button>
+        <button class="generate-new-quote cool-button"> Get a New quote</button>
       </div>
     `;
   }
@@ -143,7 +143,7 @@ function generateCard(quote) {
   }
   
   // Function to get a random quote
-  function getRandomQuote() {
+  const getRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * responses.length);
     return responses[randomIndex];
   }
