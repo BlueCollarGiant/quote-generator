683240f7a73eefefe0d4fbdc457031b61270802f
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-03-03 14:38:38 -0600

    fixed a couple bugs setting up for presentaion

--- javascript/state.js
+++ javascript/state.js
@@ -5,4 +5,5 @@ export const state = {
   inactiveQuotes: [],
   pendingQuotes: JSON.parse(localStorage.getItem("pendingQuotes")) || [],
   currentView: "welcome",
+  previousView: null,     // Previous view (to track where we came from)
 };
